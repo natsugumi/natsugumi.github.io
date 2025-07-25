ec78e7ffe5369fd2151f05962c02f2789d19e2bf
natsugumi <222600307+natsugumi@users.noreply.github.com>
2025-07-25 10:23:37 +0900

    fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "natsugumi.github.io",
+  site: "https://natsugumi.github.io",
 });
